fb69bc8eba99a83bfedc2ec329d4b45c4f7e554b
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-06-10 21:18:15 +0900

    adf

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const odb = require("oracledb");
 
 const config = {
   user: "d201802145",
-
   password: "9378",
-
-  connectString: "0.0.0.0/xe",
+  connectString: "localhost:1521/xe",
 };
 
 const app = express();
@@ -17,30 +15,55 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
+  odb.getConnection(config, function (err, connection) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute("SELECT * FROM TEAM", function (err, result) {
+      if (err) {
+        console.error(err.message);
+        doRelease(connection);
+        return;
+      }
+      console.log(result.metaData);
+      console.log(result.rows);
+
+      doRelease(connection);
+      return res.status(200).send(result);
+    });
+  });
   return res.status(200).send("hing");
 });
 
 app.post("/login", (req, res) => {
   return res.status(200).send();
 });
+
 app.post("/book", (req, res) => {
   return res.status(200).send();
 });
+
 app.get("/list", (req, res) => {
   return res.status(200).send();
 });
+
 app.get("/schedule", (req, res) => {
   return res.status(200).send();
 });
+
 app.get("/booklist", (req, res) => {
   return res.status(200).send();
 });
+
 app.get("/runtime", (req, res) => {
   return res.status(200).send();
 });
+
 app.get("/userlist", (req, res) => {
   return res.status(200).send();
 });
+
 app.post("/cancel", (req, res) => {
   return res.status(200).send();
 });
